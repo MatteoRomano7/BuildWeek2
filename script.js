ae6c43265d575b6101df58018abb5ee3088bbcb0
Giuseppe Conza <148717831+SoupKKK@users.noreply.github.com>
2023-12-11 12:40:33 +0100

    zz

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e851cdc811mshe7e16f349b95bfep185932jsn67fb1834f7cb',
+		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
