06e5650a81c641fd8564970efb84c8f6f980cc8f
“giuseppe <giuseppeforce36@email.com>
2023-12-12 09:33:36 +0100

    progress-bar

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-const url = 'https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'e851cdc811mshe7e16f349b95bfep185932jsn67fb1834f7cb',
-		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
-	}
-};
+//PROGRESS BAR PLAYER
+const progressElement = document.getElementById('progress');
+const timerElement = document.querySelector('.timer');
+const durationElement = document.querySelector('.duration');
+let currentTime = 0; 
+const duration = 20; // DURATION SONG IN SECONDS 
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+function advanceProgressBar() {
+  currentTime += 1;
+  if (currentTime <= duration) {
+    const progressWidth = (currentTime / duration) * 100;
+    progressElement.style.width = `${progressWidth}%`;
+
+  
+    const minutes = Math.floor(currentTime / 60);
+    const seconds = currentTime % 60;
+    const formattedTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    
+   
+    timerElement.textContent = formattedTime;
+
+   
+    const totalMinutes = Math.floor(duration / 60);
+    const totalSeconds = duration % 60;
+    const formattedDuration = `${totalMinutes}:${totalSeconds < 10 ? '0' : ''}${totalSeconds}`;
+   
+    durationElement.textContent = formattedDuration;
+  } else {
+    clearInterval(interval);
+  }
 }
+
+const interval = setInterval(advanceProgressBar, 1000); // SET TIME FOR PROGREBARR ADVANCEMENT IN MS
+
+
