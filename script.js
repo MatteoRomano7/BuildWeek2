08bdd75a16ecc6f716c0363f1b53c0376967fa4e
MatteoRomano7 <ezmatteo@aol.com>
2023-12-12 17:44:10 +0100

    aggiunto fetch API

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const url = "https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem";
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a605a3896bmsh1507f4a8a062864p1562ecjsn9f02d02f8e99",
+    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+  },
+};
+
+fetchApi(url, options);
+
+async function fetchApi(url, option) {
+  try {
+    const response = await fetch(url, option);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    alert("Errore nel caricamento");
+  }
+}
+
 //Dropdown Header
 const dropdown = document.querySelector(".dropdown");
 const dropdownMenu = document.querySelector(".dropdownMenu");
