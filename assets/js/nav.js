e3c0ca20f7f68ed5f4bafe8e8f78f5fa6df49e2c
Valk <bznand@gmail.com>
2023-12-12 12:51:42 +0100

    finito stile navbar

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,3 +1,5 @@
+import { playlists } from "../js/playlists.js";
+
 const endpoint = "https://deezerdevs-deezer.p.rapidapi.com/";
 const options = {
   method: "GET",
@@ -8,39 +10,50 @@ const options = {
 };
 
 function pass() {
-  return
+  return;
 }
-
-let counter = 0
-while (counter <50) {
-  generatePlaylists()
-
-}
-
 
 function generatePlaylists() {
-  fetch(`${endpoint}playlist/${random(3000)}}`, options).then(response => response.json()).then(data => {
-    try {
-      if (data.error.code) {
-        console.log('pepe')
-        return
-      }
-    } catch (DataException){
-      pass()
-    }
-    const playlistBox = document.querySelector('.auto-playlists')
+  const playlistBox = document.querySelector(".auto-playlists");
+  for (let i = 0; i < 60; i++) {
+    let playlist = playlists[random(7100)];
+
     playlistBox.innerHTML += `
-    <li><a href="#">${data.title}</a></li>
-    `
-    counter++
-    console.log(counter)
-
-  })
+    
+    <div><li><a href="#" title="${playlist}">${playlist}</a></li></div>
+    `;
   }
-
-
-function random(n) {
-    return Math.round(Math.random() * n)
 }
 
-generatePlaylists()
+function dots() {
+  const playlistLinks = document.querySelectorAll(".auto-playlists a");
+  for (let elem of playlistLinks) {
+    if (elem.scrollWidth > 270) {
+      const dot = document.createElement("svg");
+      dot.innerHTML += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+      <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3"/>
+    </svg>`;
+    elem.addEventListener('mouseover', () => {
+      dot.classList.toggle('playlist-dots-vanish')
+    })
+    elem.addEventListener('mouseleave', () => {
+      dot.classList.toggle('playlist-dots-vanish')
+    })
+
+    elem.classList.add('playlist-hover')
+    elem.parentElement.after(dot)
+    }
+  }
+}
+
+function random(n) {
+  return Math.round(Math.random() * n) + 1;
+}
+
+const tansitionTimePerPixel = 0.01;
+const textBoxes = document.querySelectorAll(
+  ".auto-playlists li"
+);
+
+generatePlaylists();
+dots();
