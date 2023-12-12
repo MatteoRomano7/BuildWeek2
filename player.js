ff27f215fa0f0588c9311e4fd32ce673d4d7cdec
“giuseppe <giuseppeforce36@email.com>
2023-12-12 12:48:09 +0100

    final desktop

--- player.js
+++ player.js
@@ -24,7 +24,7 @@ const playIcon = document.getElementById('play-icon')
 const stopIcon = document.getElementById('stop-icon')
 
 let currentTime = 0
-const duration = 20 //DURATION DEL TIMER IN SECONDS
+const duration = 200 //DURATION DEL TIMER IN SECONDS
 let isPlaying = false //FALSE PER FARLO INZIARE DA SUBITO TRUE PER L INVERSO
 let interval
 
@@ -129,6 +129,59 @@ function updateProgress(event) {
 
   timerElement.textContent = formattedTime
 }
+//CENTRAL PLAYER BTN REPEATER & SHUFFLE
+const shuffle = document.getElementById("shuffle");
+let isGreen = false;
+
+function shuffleBtn() {
+  if (!isGreen) {
+    shuffle.style.fill = "#1db954";
+    shuffle.style.borderBottom = "1px solid #1db954"
+    isGreen = true;
+  } else {
+    shuffle.style.fill = "";
+    shuffle.style.borderBottom = ""
+    isGreen = false;
+  }
+}
+
+shuffle.addEventListener('click', shuffleBtn);
+
+
+const repeater = document.getElementById("repeater");
+let clickCount = 0;
+
+function repeaterBtn() {
+  clickCount++;
+  
+  if (clickCount === 1) {
+    repeater.style.fill = "#1db954";
+    repeater.style.borderBottom = "1px solid #1db954"
+  } else if (clickCount === 2) {
+    repeater.innerHTML= `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#1db954" class="bi bi-repeat-1" viewBox="0 0 16 16">
+    <path d="M11 4v1.466a.25.25 0 0 0 .41.192l2.36-1.966a.25.25 0 0 0 0-.384l-2.36-1.966a.25.25 0 0 0-.41.192V3H5a5 5 0 0 0-4.48 7.223.5.5 0 0 0 .896-.446A4 4 0 0 1 5 4zm4.48 1.777a.5.5 0 0 0-.896.446A4 4 0 0 1 11 12H5.001v-1.466a.25.25 0 0 0-.41-.192l-2.36 1.966a.25.25 0 0 0 0 .384l2.36 1.966a.25.25 0 0 0 .41-.192V13h6a5 5 0 0 0 4.48-7.223Z"/>
+    <path d="M9 5.5a.5.5 0 0 0-.854-.354l-1.75 1.75a.5.5 0 1 0 .708.708L8 6.707V10.5a.5.5 0 0 0 1 0z"/>
+  </svg>`
+
+
+    repeater.appendChild(number);
+  } else if (clickCount === 3) {
+    repeater.style.fill = ""; 
+    repeater.style.borderBottom = ""
+    const number = repeater.querySelector("p");
+    repeater.innerHTML = `<svg id="repeater" xmlns="http://www.w3.org/2000/svg"  fill="white" class="bi bi-repeat" viewBox="0 0 16 16">
+    <path d="M11 5.466V4H5a4 4 0 0 0-3.584 5.777.5.5 0 1 1-.896.446A5 5 0 0 1 5 3h6V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192Zm3.81.086a.5.5 0 0 1 .67.225A5 5 0 0 1 11 13H5v1.466a.25.25 0 0 1-.41.192l-2.36-1.966a.25.25 0 0 1 0-.384l2.36-1.966a.25.25 0 0 1 .41.192V12h6a4 4 0 0 0 3.585-5.777.5.5 0 0 1 .225-.67Z"/>
+  </svg>`
+    if (number) {
+      repeater.removeChild(number);
+    }
+    clickCount = 0;
+  }
+}
+
+repeater.addEventListener('click', repeaterBtn);
+
+
 
 
 //RIGHT VOLUME PROGRESS BAR
