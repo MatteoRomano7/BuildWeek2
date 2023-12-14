613916afbc619e9e708b54551aac7fe06311b41b
Franncesco Caputo <caputo.francesco001@gmail.com>
2023-12-14 15:37:58 +0100

    js finale sidebarfriends

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,7 +1,32 @@
-function toggleSidebar() {
-  let sidebar = document.querySelector(".activitySection.sidebar");
-  sidebar.classList.toggle("minimized");
+const minimizeFriends = document.querySelector(".bi-x-lg");
+const friends = document.querySelector("#friends");
+const activityItems = document.querySelectorAll(".activityItem");
+const openFriends = document.querySelector(".openFriends");
+
+function closeFriendsSide() {
+  let sidebar = document.querySelector(".activitySection");
+  sidebar.style.minWidth = "50px";
+  sidebar.style.maxWidth = "50px";
+  friends.style.display = "none";
+  openFriends.style.display = "block";
+
+  activityItems.forEach((content) => {
+    content.style.display = "none";
+  });
 }
 
-var minimizeIcon = document.querySelector(".bi-x-lg");
-minimizeIcon.addEventListener("click", toggleSidebar);
+function openFriendsSide() {
+  let sidebar = document.querySelector(".activitySection");
+  sidebar.style.minWidth = "250px";
+  sidebar.style.maxWidth = "250px";
+  friends.style.display = "flex";
+  openFriends.style.display = "none";
+
+  activityItems.forEach((content) => {
+    setTimeout(() => {
+      content.style.display = "flex";
+    }, 150);
+  });
+}
+openFriends.addEventListener("click", openFriendsSide);
+minimizeFriends.addEventListener("click", closeFriendsSide);
