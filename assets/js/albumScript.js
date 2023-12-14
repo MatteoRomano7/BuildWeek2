8237c817ab15bcfa2c0a8f4953e23cf9a97f887f
MethaKinde <meethabeelkinde@gmail.com>
2023-12-14 12:09:31 +0100

    fixing bug

--- assets/js/albumScript.js
+++ assets/js/albumScript.js
@@ -15,3 +15,16 @@ dropdown.addEventListener('click', () => {
         isDropdownVisible = 'hidden'
     }
 })
+
+/*GESTIONE TESTI OVERFLOW*/
+// Seleziona tutti gli h4
+const h4Elements = document.querySelectorAll('h4');
+
+// Cicla attraverso gli h4
+h4Elements.forEach(function (h4) {
+    // Verifica se l'h4 ha un overflow
+    if (h4.scrollWidth > h4.clientWidth) {
+        // Applica la classe overflowing solo se c'è overflow
+        h4.classList.add('overflowing');
+    }
+});
